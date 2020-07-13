c6074edf6913b7b6e95499ad43c7e27e1b6adf43
CUST_05 <zhangcaiqi888@126.com>
2020-07-13 14:46:32 +0800

    1.懒加载实现

--- commonlib/src/main/java/com/xingqi/code/commonlib/base/BaseLazyLoadFragment.java
+++ commonlib/src/main/java/com/xingqi/code/commonlib/base/BaseLazyLoadFragment.java
@@ -1,35 +1,16 @@
 package com.xingqi.code.commonlib.base;
 
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 
 import com.xingqi.code.commonlib.mvp.BasePresenter;
 
-import java.util.List;
 
 
-/**
- * 子类覆写{@link BaseLazyLoadFragment}lazyLoadData可快速实现Fragment懒加载
- */
 public abstract class BaseLazyLoadFragment<P extends BasePresenter> extends BaseFragment<P> {
 
 
-    private boolean isViewCreated; // 界面是否已创建完成
-    private boolean isVisibleToUser; // 是否对用户可见
     private boolean isDataLoaded; // 数据是否已请求
 
-    /**
-     * 第一次可见时触发调用,此处实现具体的数据请求逻辑
-     */
-    protected abstract void lazyLoadData();
-
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        this.isVisibleToUser = isVisibleToUser;
-        tryLoadData();
-    }
 
     /**
      * 保证在initData后触发
@@ -37,40 +18,16 @@ public abstract class BaseLazyLoadFragment<P extends BasePresenter> extends Base
     @Override
     public void onResume() {
         super.onResume();
-        isViewCreated = true;
-        tryLoadData();
-    }
-
-    /**
-     * ViewPager场景下，判断父fragment是否可见
-     */
-    private boolean isParentVisible() {
-        Fragment fragment = getParentFragment();
-        return fragment == null || (fragment instanceof BaseLazyLoadFragment && ((BaseLazyLoadFragment) fragment).isVisibleToUser);
-    }
-
-    /**
-     * ViewPager场景下，当前fragment可见时，如果其子fragment也可见，则让子fragment请求数据
-     */
-    private void dispatchParentVisibleState() {
-        FragmentManager fragmentManager = getChildFragmentManager();
-        List<Fragment> fragments = fragmentManager.getFragments();
-        if (fragments.isEmpty()) {
-            return;
-        }
-        for (Fragment child : fragments) {
-            if (child instanceof BaseLazyLoadFragment && ((BaseLazyLoadFragment) child).isVisibleToUser) {
-                ((BaseLazyLoadFragment) child).tryLoadData();
-            }
-        }
-    }
-
-    public void tryLoadData() {
-        if (isViewCreated && isVisibleToUser && isParentVisible() && !isDataLoaded) {
-            lazyLoadData();
+        if(!isDataLoaded){
+            lazyLoad();
             isDataLoaded = true;
-            //通知子Fragment请求数据
-            dispatchParentVisibleState();
         }
     }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        isDataLoaded = false;
+    }
+    public abstract void lazyLoad();
+
 }
