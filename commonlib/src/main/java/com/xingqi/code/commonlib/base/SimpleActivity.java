ca81a41c9f7d62cd23efef2d00b022753359d1a2
a3 <zhangcaiqi888@126.com>
2020-11-06 16:17:52 +0800

    1.增加SimpleActivity

---
+++ commonlib/src/main/java/com/xingqi/code/commonlib/base/SimpleActivity.java
@@ -0,0 +1,28 @@
+package com.xingqi.code.commonlib.base;
+
+import com.xingqi.code.commonlib.mvp.BasePresenter;
+
+public abstract class SimpleActivity<P extends BasePresenter> extends BaseActivity<P> {
+
+    @Override
+    public boolean isRootPage() {
+        return false;
+    }
+
+
+
+    @Override
+    public boolean hasToolbar() {
+        return false;
+    }
+
+    @Override
+    public String toolbarTitle() {
+        return null;
+    }
+
+    @Override
+    public void release() {
+
+    }
+}
